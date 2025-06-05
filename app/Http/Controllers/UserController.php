924c9e9f06b18719f5de32366fe6c893138da703
okochi-mika <hiyoko_m@icloud.com>
2025-06-05 23:11:00 +0900

    ファイルを追加

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,53 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+        public function mypage()
     {
-        //
+        $user = Auth::user();
+
+        return view('users.mypage', compact('user'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function show(User $user)
-    {
-        //
-    }
-
-    /**
+       /**
      * Show the form for editing the specified resource.
      *
      * @param  \App\Models\User  $user
@@ -57,7 +23,9 @@ class UserController extends Controller
      */
     public function edit(User $user)
     {
-        //
+        $user = Auth::user();
+
+        return view('users.edit', compact('user'));
     }
 
     /**
@@ -69,17 +37,47 @@ class UserController extends Controller
      */
     public function update(Request $request, User $user)
     {
-        //
+        $user = Auth::user();
+        
+        $user->name = $request->input('name') ? $request->input('name') : $user->name;
+        $user->email = $request->input('email') ? $request->input('email') : $user->email;
+        $user->postal_code = $request->input('postal_code') ? $request->input('postal_code') : $user->postal_code;
+        $user->address = $request->input('address') ? $request->input('address') : $user->address;
+        $user->phone = $request->input('phone') ? $request->input('phone') : $user->phone;
+        $user->update();
+
+        return to_route('mypage');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(User $user)
+    public function update_password(Request $request)
     {
-        //
+        $validatedData = $request->validate([
+            'password' => 'required|confirmed',
+        ]);
+
+        $user = Auth::user();
+
+        if ($request->input('password') == $request->input('password_confirmation')) {
+            $user->password = bcrypt($request->input('password'));
+            $user->update();
+        } else {
+            return to_route('mypage.edit_password');
+        }
+
+        return to_route('mypage');
+    }
+
+    public function edit_password()
+    {
+        return view('users.edit_password');
+    }
+
+        public function favorite()
+    {
+        $user = Auth::user();
+
+        $favorite_products = $user->favorite_products;
+
+        return view('users.favorite', compact('favorite_products'));
     }
 }
