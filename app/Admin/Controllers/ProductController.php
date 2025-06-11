faecb7606a147d34de85f6044b878715e6a6c06f
okochi-mika <hiyoko_m@icloud.com>
2025-06-11 15:38:35 +0900

    ファイルの追加

--- app/Admin/Controllers/ProductController.php
+++ app/Admin/Controllers/ProductController.php
@@ -27,14 +27,23 @@ class ProductController extends AdminController
     {
         $grid = new Grid(new Product());
 
-        $grid->column('id', __('Id'));
+        $grid->column('id', __('Id'))->sortable();
         $grid->column('name', __('Name'));
         $grid->column('description', __('Description'));
-        $grid->column('price', __('Price'));
+        $grid->column('price', __('Price'))->sortable();
         $grid->column('category.name', __('Category Name'));
         $grid->column('image', __('Image'))->image();
-        $grid->column('created_at', __('Created at'));
-        $grid->column('updated_at', __('Updated at'));
+        $grid->column('recommend_flag', __('Recommend Flag'));
+        $grid->column('created_at', __('Created at'))->sortable();
+        $grid->column('updated_at', __('Updated at'))->sortable();
+
+        $grid->filter(function($filter) {
+            $filter->like('name', '商品名');
+            $filter->like('description', '商品説明');
+            $filter->between('price', '金額');
+            $filter->in('category_id', 'カテゴリー')->multipleSelect(Category::all()->pluck('name', 'id'));
+            $filter->equal('recommend_flag', 'おすすめフラグ')->select(['0' => 'false', '1' => 'true']);
+        });
 
         return $grid;
     }
@@ -55,6 +64,7 @@ class ProductController extends AdminController
         $show->field('price', __('Price'));
         $show->field('category.name', __('Category Name'));
         $show->field('image', __('Image'))->image();
+        $show->field('recommend_flag', __('Recommend Flag'));
         $show->field('created_at', __('Created at'));
         $show->field('updated_at', __('Updated at'));
 
@@ -75,6 +85,7 @@ class ProductController extends AdminController
         $form->number('price', __('Price'));
         $form->select('category_id', __('Category Name'))->options(Category::all()->pluck('name', 'id'));
         $form->image('image', __('Image'));
+        $form->switch('recommend_flag', __('Recommend Flag'));
 
         return $form;
     }
