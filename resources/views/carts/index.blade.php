215fdfeefee4b2315b6c6055998601fee7b0e65f
okochi-mika <hiyoko_m@icloud.com>
2025-06-15 14:55:08 +0900

    ファイルの追加

--- resources/views/carts/index.blade.php
+++ resources/views/carts/index.blade.php
@@ -45,61 +45,30 @@
        </div>
 
        <hr>
-
-       <div class="offset-8 col-4">
-          <div class="row">
-             <div class="col-6">
-                <h2>送料</h2>
+               <div class="offset-8 col-4">
+            <div class="row">
                 <div class="col-6">
-                    <h2>￥{{ $carriage_cost }}</h2>
+                    <h2>合計</h2>
                 </div>
-           </div>
-        </div>
-
-       <div class="offset-8 col-4">
-           <div class="row">
-               <div class="col-6">
-                   <h2>合計</h2>
-               </div>
-               <div class="col-6">
-                   <h2>￥{{$total}}</h2>
-               </div>
-               <div class="col-12 d-flex justify-content-end">
-                   表示価格は税込みです
-               </div>
-           </div>
-    </div>
-
-    <form method="post" action="{{route('carts.destroy')}}" class="d-flex justify-content-end mt-3">
-        @csrf
-        <input type="hidden" name="_method" value="DELETE">
-        <a href="{{route('top')}}" class="btn samuraimart-favorite-button border-dark text-dark mr-3">
-            買い物を続ける
-        </a>
-        @if ($total > 0)
-        <div class="btn samuraimart-submit-button" data-bs-toggle="modal" data-bs-target="#buy-confirm-modal">購入を確定する</div>
-        @else
-        <div class="btn samuraimart-submit-button disabled" data-bs-toggle="modal" data-bs-target="#buy-confirm-modal">購入を確定する</div>
-        @endif
-
-        <div class="modal fade" id="buy-confirm-modal" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="staticBackdropLabel">購入を確定しますか？</h5>
-                            <button type="button" class="close" data-bs-dismiss="modal" aria-label="閉じる">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn samuraimart-favorite-button border-dark text-dark" data-bs-dismiss="modal">閉じる</button>
-                            <button type="submit" class="btn samuraimart-submit-button">購入</button>
-                        </div>
-                    </div>
+                <div class="col-6">
+                    <h2>￥{{$total}}</h2>
+                </div>
+                <div class="col-12 d-flex justify-content-end">
+                    表示価格は税込みです
                 </div>
             </div>
-    </from>
+        </div>
+
+        <div class="d-flex justify-content-end mt-3">
+            <a href="{{route('top')}}" class="btn samuraimart-favorite-button border-dark text-dark mr-3">
+                買い物を続ける
+            </a>
+            @if ($total > 0)
+                <a href="{{ route('checkout.index') }}" class="btn samuraimart-submit-button">購入に進む</a>
+            @else
+                <button class="btn samuraimart-submit-button disabled">購入に進む</button>
+            @endif
+        </div>
     </div>
 </div>
-
 @endsection
