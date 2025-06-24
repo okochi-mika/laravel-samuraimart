e8d504d9c8401d86b5651fd5fe995086d097986a
okochi-mika <hiyoko_m@icloud.com>
2025-06-24 13:19:41 +0900

    UserSeeder追加

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,24 +4,27 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
-        DB::table('users')->insert([
-            'name' => 'test',
-            'email' => 'test2@email.com',
-            'email_verified_at' => now(),
-            'password' => bcrypt('password'),
-            'address' => 'test',
-        ]);
+        $user = new User();
+        $user->name = "太郎";
+        $user->email = 'tarou@example.com';
+        $user->email_verified_at = Carbon::now();
+        $user->password = Hash::make('password');
+        $user->postal_code = "0000000";
+        $user->address = "東京都";
+        $user->phone = "000-0000-0000";
+        $user->save();
     }
     
 }
